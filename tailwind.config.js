fe066d759b3ff3293174d7721c010d9d440501b3
Ayan7071 <ayansayyad010@gmail.com>
2026-02-22 21:27:11 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,html}" // apne project ke files ke according adjust karo
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
